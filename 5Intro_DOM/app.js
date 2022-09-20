1795cb97a285ed23ab1660a16fef51915ba15980
Harshitr10 <1012harshit@gmail.com>
2022-09-20 23:44:31 +0530

    added some featres

--- 5Intro_DOM/app.js
+++ 5Intro_DOM/app.js
@@ -11,4 +11,37 @@ for(let img of all_images)
 {
  
     console.log(img.src)
+    
 }
+
+// document.querySelector('h1')
+// it always selects the first element h1 you can select all as an array using document.querySelectorAll
+
+document.querySelector('a').title
+const firstLink  = document.querySelector('a')
+firstLink.href//same
+firstLink.getAttribute('href');//same
+
+firstLink.getAttribute('title')
+firstLink.setAttribute('href','http://www.google.com')
+
+//changing styles
+
+const allLinks = document.querySelector('a');
+for(let links of allLinks)
+{
+    links.style.color ='rgb(0,108,134)'
+    links.style.textDecorationColor = 'magenta';
+    links.style.textDecorationStyle ='wavy';
+}
+
+
+// all changes are applied in the code as inline styles
+window.getComputedStyle(h1).color
+window.getComputedStyle(h1).fontSize
+window.getComputedStyle(h1).fontFamily
+window.getComputedStyle(h1).margin
+// get all CSS style applied at tht element
+
+
+
